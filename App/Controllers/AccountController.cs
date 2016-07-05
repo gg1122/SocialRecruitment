b979fba435bfefb360069b7576bb4c2df8896a24
clark <clark@DESKTOP-HNNPKIR>
2016-07-05 10:15:39 +0800

    GetIsLogining增加了

---
+++ App/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Models;
+namespace Langben.App.Controllers
+{
+    /// <summary>
+    /// 会员
+    /// </summary>
+    public class AccountController : BaseController
+    {
+        /// <summary>
+        /// 是否登录
+        /// </summary>
+        /// <returns></returns>
+        public string GetIsLogining()
+        {
+            if (CurrentAccount != null)
+            {
+                return "Y";
+            }
+            else
+            {
+                return "N";
+            }
+        }
+    }
+}
