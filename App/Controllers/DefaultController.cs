b66d5f94c159a853a5b6f1aecbccb4b39ba4b4e2
clark <clark@DESKTOP-I1SM152>
2016-09-17 18:34:54 +0800

    发布博客

--- App/Controllers/DefaultController.cs
+++ App/Controllers/DefaultController.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace Langben.App.Controllers
 {
-    public class DefaultController : ApiController
+    public class DefaultController : BaseController
     {
+        public ActionResult Index(string id)
+        {
+
+            return View();
+        }
     }
 }
 
--- App/Res/ckeditor/config.js
+++ App/Res/ckeditor/config.js
@@ -1,36 +1,27 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-
-CKEDITOR.editorConfig = function (config) {
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
     config.image_previewText = ' '; //预览区域显示内容
     config.filebrowserImageUploadUrl = "admin/UserArticleFileUpload.do"; //待会要上传的action或servlet
-
     config.toolbarGroups = [
-          { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
-            { name: 'paragraph', groups: ['list'] },
-		{ name: 'insert' },
-		{ name: 'tools' }
+            { name: 'basicstyles', groups: ['basicstyles'] },
+              { name: 'paragraph', groups: ['list'] },
+          { name: 'insert' },
+          { name: 'tools' }
     ];
-   
-    config.extraPlugins = 'codesnippet';
 
-    // 取消“拖拽以改变尺寸”功能 plugins / resize / plugin.js
-    // config.resize_enabled = false;
-    config.entities_greek = true; // 是否转换一些难以显示的字符为相应的 HTML字符 
-    //config.fontSize_defaultLabel = '12px';
-    config.language = 'zh-cn';
-    //工具栏默认是否展开
-    config.toolbarStartupExpanded = true;
-    // config.toolbarCanCollapse = true;
-    // Remove some buttons, provided by the standard plugins, which we don't
-    // need to have in the Standard(s) toolbar.
 
-    config.enterMode = CKEDITOR.ENTER_BR;
-    config.shiftEnterMode = CKEDITOR.ENTER_P;
-    config.removeButtons = 'Iframe,HorizontalRule,Table,IFrame,Flash,Smiley,SpecialChar,PageBreak,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+    config.removeButtons = 'Italic,Table,Anchor,SpecialChar,HorizontalRule,Underline,Strike,Subscript,Superscript';
+      
 
-    config.removeDialogTabs = 'image:advanced;link:advanced';
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 };
