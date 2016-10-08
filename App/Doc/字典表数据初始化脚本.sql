05b4adda0cc9e55eda91c3ff02675d809cd1cfd3
hilary <hilary@DESKTOP-2JQFSTB>
2016-10-08 21:55:12 +0800

    完整度数据字典脚本

---
+++ App/Doc/字典表数据初始化脚本.sql
@@ -0,0 +1,10 @@
+----------------------------------------------------------------------------------------------------
+--完整度比例初始化
+insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2487','20',null,'Resume','Account')
+insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2488','20',null,'Resume','DegreeSchool')
+insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2489','10',null,'Resume','LanguageCompetence')
+insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2490','10',null,'Resume','ITAbility')
+insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2491','20',null,'Resume','ProjectExperience')
+insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2492','10',null,'Resume','InternshipExperience')
+insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2493','10',null,'Resume','FileUploader')
+----------------------------------------------------------------------------------------------------
