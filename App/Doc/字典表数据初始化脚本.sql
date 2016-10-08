2f5005296f80403facca161dbf772ac4e9487b01
hilary <hilary@DESKTOP-2JQFSTB>
2016-10-08 22:08:04 +0800

    完整度

--- App/Doc/字典表数据初始化脚本.sql
+++ App/Doc/字典表数据初始化脚本.sql
@@ -3,8 +3,7 @@
 insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2487','20',null,'Resume','Account')
 insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2488','20',null,'Resume','DegreeSchool')
 insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2489','10',null,'Resume','LanguageCompetence')
-insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2490','10',null,'Resume','ITAbility')
+insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2490','20',null,'Resume','ITAbility')
 insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2491','20',null,'Resume','ProjectExperience')
 insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2492','10',null,'Resume','InternshipExperience')
-insert into SysField(id,MyTexts,ParentId,MyTables,MyColums) values('2493','10',null,'Resume','FileUploader')
 ----------------------------------------------------------------------------------------------------
